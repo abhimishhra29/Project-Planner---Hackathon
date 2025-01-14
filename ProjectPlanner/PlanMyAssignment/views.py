d43d9260f6a6759e985eb0b65023b5f4e4d8bf3d
s3934581 <116268296+s3934581@users.noreply.github.com>
2025-01-14 16:35:52 +0530

    coomit1

--- ProjectPlanner/PlanMyAssignment/views.py
+++ ProjectPlanner/PlanMyAssignment/views.py
@@ -271,7 +271,7 @@ def add_reviewer(request):
             ]
         )
         llm = ChatOpenAI(
-            model="gpt-4",  # Specify the model you want to use
+            model="gpt-4o-mini",  # Specify the model you want to use
             temperature=0.7  # Adjust the temperature based on your needs
         )
         tools = [question_retriever] + [solution_retriever] + [retriever_require_grading] + [rubric_retriever]
